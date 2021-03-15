9eb494b1e8059ff0c3b0e7bef048ca91d79756e9
audo <adam.aurisch@gmail.com>
2021-03-15 17:00:56 +1000

    update

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/audoman/adamsmonero.git
+git clone https://github.com/audoman/adamsmonero
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
